b446d841f3b50c6d18604dea42a1d8e43a5e1ee6
NaderMoa <nadermoazezi@gmail.com>
2024-01-19 15:08:35 -0500

    creating flask

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,9 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 
 #################################################
@@ -7,22 +11,34 @@
 #################################################
 
 
+
+
 # reflect an existing database into a new model
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return(
+        f"Welcome to SQLalchemy challenge!<br/>"
+        f"here are the available routes:<br/>"
+        f"/api/v1/static_Route <br/>"
+        f"/api/v1/dynamic_Route"
 
-
+    )
 
 
 #################################################
