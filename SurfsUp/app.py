2c3c0dac53fb403c65b61d47ce9c25c4b0675e3d
NaderMoa <nadermoazezi@gmail.com>
2024-01-19 17:33:58 -0500

    creating static route app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -35,8 +35,9 @@ def index():
     return(
         f"Welcome to SQLalchemy challenge!<br/>"
         f"here are the available routes:<br/>"
-        f"/api/v1/static_Route <br/>"
-        f"/api/v1/dynamic_Route"
+        f"/api/v1.0/precipitation <br/>"
+        f"/api/v1.0/stations <br/>"
+        f"/api/v1.0/tobs <br/>"
 
     )
 
@@ -44,3 +45,36 @@ def index():
 #################################################
 # Flask Routes
 #################################################
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    precipitation_result = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= "2016_08_23").all()
+    session.close()
+    precipitation_output = []
+    for date, prcp in precipitation_result:
+        prec_dict = {}
+        prec_dict["date"] = prcp
+        precipitation_output.append(prec_dict)
+
+    return jsonify(precipitation_output)
+
+@app.route("/api/v1.0/stations")
+def station():
+    station_list = session.query(Station.name).all()
+    session.close()
+    station_output = []
+    for station in station_list:
+        station_output.append(station.name)
+    return jsonify(station_output)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    tobs_list = session.query(Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date >= "2016_08_23").all()
+    session.close()
+    tobs_output = []
+    for tobs in tobs_list:
+        tobs_output.append(tobs.tobs)
+    return jsonify(tobs_output)
+
+
+
+app.run()
