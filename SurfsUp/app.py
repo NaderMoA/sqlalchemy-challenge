79f8053536f4c7a1aca1e6de8d3e7bd76255f1b6
NaderMoa <nadermoazezi@gmail.com>
2024-01-19 20:41:35 -0500

    adding the dynamic routes to the display welcome page and list of routes

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -38,6 +38,8 @@ def index():
         f"/api/v1.0/precipitation <br/>"
         f"/api/v1.0/stations <br/>"
         f"/api/v1.0/tobs <br/>"
+        f"/api/v1.0/<start> <br/>"
+        f"/api/v1.0/<start>/<end>"
 
     )
 
